f1235d09c41f6854220444b78ffcbf85114ff081
Vitor Serpa <vitorserpa123@gmail.com>
2025-12-03 09:47:14 -0300

    feat(Wireframe página inicial feita)

--- kolty-page/app/layout.tsx
+++ kolty-page/app/layout.tsx
@@ -24,6 +24,22 @@ export default function RootLayout({
           </nav>
         </div>
         {children}
+        <footer className="footer">
+          <div className="bndes">
+            <h4>Icone BNDES</h4>
+            <div className="bndes-text">
+              <p><b>Entre em contato e solicete seu orçamento!</b></p>
+              <h3>Pague em até 48X  utilizando o BNDES</h3>
+            </div>
+          </div>
+          <div className="redes-sociais">
+            <h2>Acompanhe nossas redes sociais</h2>
+            <h4>Icone Face e Instagram</h4>
+          </div>
+          <div className="direitos-reservados">
+            <p>© 2021 Kolty Engenharia.</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- kolty-page/app/page.tsx
+++ kolty-page/app/page.tsx
@@ -1,26 +1,39 @@
 export default function Home() {
     return (
-        <main className="main-container">
-            <p>
-                A <b>KOLTY</b> Engenharia é uma empresa brasileira que
-                desenvolve e executa projetos, a partir do zero e de acordo com
-                o pedido do cliente e suas ideias, de máquinas de produção de
-                médio e grande porte para usinagem e cortes. Além de oferecer
-                manutenção e upgrades retrofits. Atuando no mercado desde 2011,
-                com máquinas operando em mais de 10 estados, conta com uma
-                equipe altamente qualificada de projetistas, técnicos e
-                engenheiros que, com soluções personalizadas para problemas
-                industriais, atende as necessidades de indústrias e de pessoas
-                físicas.
-            </p>
+        <main className="main-div">
+            <div className="main-container">
+                <p>
+                    A <b>KOLTY</b> Engenharia é uma empresa brasileira que
+                    desenvolve e executa projetos, a partir do zero e de acordo com
+                    o pedido do cliente e suas ideias, de máquinas de produção de
+                    médio e grande porte para usinagem e cortes. Além de oferecer
+                    manutenção e upgrades retrofits. Atuando no mercado desde 2011,
+                    com máquinas operando em mais de 10 estados, conta com uma
+                    equipe altamente qualificada de projetistas, técnicos e
+                    engenheiros que, com soluções personalizadas para problemas
+                    industriais, atende as necessidades de indústrias e de pessoas
+                    físicas.
+                </p>
 
-            <h1>Especialidades</h1>
-            <p>
-                Máquinas CNC em geral // Automatização de tornos, fresadoras e
-                retífica // Concepção total de máquina de corte a plasma //
-                Fresadoras CNC do tipo router // Projetos especiais
-                computadorizados a partir do zero.
-            </p>
+                <h1>Especialidades</h1>
+                <p>
+                    Máquinas CNC em geral // Automatização de tornos, fresadoras e
+                    retífica // Concepção total de máquina de corte a plasma //
+                    Fresadoras CNC do tipo router // Projetos especiais
+                    computadorizados a partir do zero.
+                </p>
+            </div>
+            <div className="carrousel-imagens">
+                <h3>Carrousel de imagens aqui</h3>
+            </div>
+            <div className="inicio-kolty-div">
+                    <h4>Icone Serra</h4>
+                <div className="inicio-kolty-text">
+                    <p>
+                    A Kolty iniciou suas atividades na Incubadora do Parque Tecnológico UNIVAP em São José dos Campos, SP. Atualmente possui sede própria na Zona Sul de São José dos Campos, SP com 1000m² de área coberta, refeitório e academia para os funcionários. 
+                    </p>
+                </div>
+            </div>
         </main>
     );
 }
